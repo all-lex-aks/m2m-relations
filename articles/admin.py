a730438990a99dfa20c6b3f8c9f4af3c9448bfcd
all-lex-aks <alexei.k.aksyutin@gmail.com>
2026-02-08 20:46:01 +0300

    Commit on Sunday without Inlines

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Tag, Scope
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'text', 'published_at', 'image']
+    list_filter = ['id', 'title', 'text', 'published_at', 'image']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['id', 'name']
+
